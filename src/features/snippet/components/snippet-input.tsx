d22a1158b54def58be587473924c12f1b3d9ce7d
Scynes <dustin.greyfield@gmail.com>
2024-08-09 12:55:20 -0400

    add: snippet-edit-code component. Need to add functionality to generating a random 4 digit code.

--- src/features/snippet/components/snippet-input.tsx
+++ src/features/snippet/components/snippet-input.tsx
@@ -2,19 +2,23 @@ import { Box, Container, Flex, TextArea, TextField } from '@radix-ui/themes';
 import { SubmitButton } from './submit-button';
 import { submitSnippet } from '../actions/submit-snippet';
 import { SelectLanguage } from './select-language';
+import { SnippetEditCode } from './snippet-edit-code';
 
 export const SnippetInput = () => {
     return (
         <Box height={ '100%' } p={ '3' }>
             <Container size={ '2' }>
-                <form action={ submitSnippet } className={ 'flex flex-col flex-1' }>
-                    <TextField.Root name={ 'title' } placeholder={ 'Title' } className={ 'w-full mb-4' } size={ '3' } />
-                    <TextArea name={ 'text' } size={ { initial: '1', sm: '3' } } className={ 'w-full mb-4 h-80' } />
+                <form action={ submitSnippet } className={ 'flex flex-col flex-1 gap-4' }>
+                    <Flex gap={ '4' } className={ 'flex-col sm:flex-row' }>
+                        <TextField.Root name={ 'title' } placeholder={ 'Title' } className={ 'w-full' } size={ '3' } />
+                        <SelectLanguage />
+                    </Flex>
+                    <TextArea name={ 'text' } size={ { initial: '1', sm: '3' } } className={ 'w-full h-80' } />
                     <Flex gap={ '4' } className={ 'transition-all flex-col-reverse sm:flex-row' }>
                         <Flex flexGrow={ '1' }>
                             <SubmitButton>Submit</SubmitButton>
                         </Flex>
-                        <SelectLanguage />
+                        <SnippetEditCode />
                     </Flex>
                 </form>
             </Container>
--- src/features/snippet/components/submit-button.tsx
+++ src/features/snippet/components/submit-button.tsx
@@ -8,7 +8,7 @@ export const SubmitButton = ( { children }: { children:  React.ReactNode } ) => 
     const { pending } = useFormStatus();
 
     return (
-        <Button type={ 'submit' } loading={ pending } size={ '3' } variant={ 'surface' } className={ '!transition-all !cursor-pointer !flex-1' }>
+        <Button type={ 'submit' } loading={ pending } size={ '3' } variant={ 'surface' } className={ 'w-full transition-all !cursor-pointer !flex-1' }>
             { children }
         </Button>
     )
