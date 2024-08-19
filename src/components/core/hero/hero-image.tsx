834f7cb90b6aa3a384b5265cc81c92d01a2bb175
Scynes <dustin.greyfield@gmail.com>
2024-08-19 00:05:27 -0400

    fix: hero image hydration mismatch.

--- src/components/core/hero/hero-image.tsx
+++ src/components/core/hero/hero-image.tsx
@@ -2,12 +2,17 @@
 
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export const HeroImage = () => {
     
     const theme = useTheme();
 
-    return (
-        <Image src={ theme.theme == 'dark' ? '/example-snippet-1.png' : '/example-snippet-3.png' } width={ 800 } height={ 400 } alt={ 'Hero' } className='absolute -right-56 z-0 rounded-md'/>
-    );
+    const [mounted, setMounted] = useState(false)
+    
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    return <Image priority src={ `/example-snippet-${ theme.theme == 'dark' || !mounted ? '1' : '3' }.png` } width={ 800 } height={ 400 } alt={ 'Hero' } className='absolute -right-56 z-0 rounded-md'/>
 }
