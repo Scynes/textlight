40809e46c383cf60ce003791c4f88c5931fa77db
Scynes <dustin.greyfield@gmail.com>
2024-08-23 09:41:46 -0400

    add: support for more languages.

--- src/features/snippet/actions/submit-snippet.ts
+++ src/features/snippet/actions/submit-snippet.ts
@@ -6,17 +6,31 @@ import { Snippet } from '../types/snippet';
 
 import ShortUniqueId from 'short-unique-id';
 
-export const submitSnippet = async (initialState: any, formData: FormData): Promise< { success: boolean, link: string } > => {
+export const submitSnippet = async (initialState: any, formData: FormData): Promise< { success: boolean, link: string, error?: string } > => {
     
     const { randomUUID } = new ShortUniqueId({ length: 7 });
+
+    // Define the maximum size for the text field (in MB)
+    const MAX_TEXT_SIZE_MB = 1;
+
+    // Calculate the maximum size for the text field (in bytes)
+    const MAX_TEXT_SIZE_BYTES = MAX_TEXT_SIZE_MB * 1024 * 1024;
+
+    // Get the text from the form data
+    const text = formData.get('text')?.toString() || '';
+
+    if (text.length === 0) return { success: false, link: 'error' };
+
+    // Calculate the size of the text in bytes
+    const textSizeBytes = new Blob([ text ]).size;
+
+    if (textSizeBytes > MAX_TEXT_SIZE_BYTES) return { success: false, link: 'Snippet exceeds 1MB limit.', error: 'Snippet exceeds 1MB limit.' };
 
     const SUPABASE = createClient();
 
     const { data, error } = await SUPABASE.from('snippet').insert({ title: formData.get('title'), text: formData.get('text'), link_id: randomUUID(), language: formData.get('language') }).select().single<Snippet>();
 
     if (error) console.log(error);
-
-    //else redirect(`/s/${ data.link_id }`);
 
     return { success: true, link: data?.link_id || 'error' };
 }
--- src/features/snippet/constants/languages.ts
+++ src/features/snippet/constants/languages.ts
@@ -1,4 +1,18 @@
 export const Languages: Language[] = [
+    { name: 'C', alias: 'c' },
+    { name: 'C#', alias: 'csharp' },
+    { name: 'C++', alias: 'cpp' },
+    { name: 'CSS', alias: 'css' },
+    { name: 'Go', alias: 'go' },
+    { name: 'HTML', alias: 'html' },
+    { name: 'Java', alias: 'java' },
     { name: 'JavaScript', alias: 'javascript' },
-    { name: 'TypeScript', alias: 'typescript' },
-]
+    { name: 'Kotlin', alias: 'kotlin' },
+    { name: 'PHP', alias: 'php' },
+    { name: 'Python', alias: 'python' },
+    { name: 'Ruby', alias: 'ruby' },
+    { name: 'Rust', alias: 'rust' },
+    { name: 'SQL', alias: 'sql' },
+    { name: 'Swift', alias: 'swift' },
+    { name: 'TypeScript', alias: 'typescript' }
+];
