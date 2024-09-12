f97897fe804f122c5d652b4668578b919d41d98e
Scynes <dustin.greyfield@gmail.com>
2024-09-12 14:48:37 -0400

    add: component to handle progress input validation for clear UX.

---
+++ src/components/shared/input-validation-checkoff.tsx
@@ -0,0 +1,21 @@
+import { Flex, Text } from '@radix-ui/themes';
+
+import { FaCheck } from "react-icons/fa";
+
+interface Properties {
+
+    // Flag for if the requirement has been validated.
+    valid: boolean
+
+    // The message to display the reequirement.
+    message: string
+}
+
+export default ( { message, valid }: Properties ) => {
+    return (
+        <Flex gap={ '1' } align={ 'center' }>
+            <FaCheck size={ 10 } className={ `${ valid ? 'text-[--accent-10]' : 'text-[--gray-8]' }` } />
+            <Text as='span' size={ { initial: '1', sm: '2' } } className={ `${ valid ? 'text-[--gray-11]' : 'text-[--gray-8]' } select-none` }>{ message }</Text>
+        </Flex>
+    );
+}
