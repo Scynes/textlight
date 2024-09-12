8ea4904acad3d89df64f69a253ee2f58f124d135
Scynes <dustin.greyfield@gmail.com>
2024-09-12 15:19:32 -0400

    fix: escaped character.

--- src/features/auth/components/verify-email-dialog.tsx
+++ src/features/auth/components/verify-email-dialog.tsx
@@ -10,7 +10,9 @@ export const VerifyEmailDialog = () => {
     return (
         <Flex direction={ 'column' } gap={ '4' } justify={ 'center' } align={ 'center' } className={ 'flex-1' }>
             <IoCheckmarkCircle size={ '4rem' } className={ 'text-[--accent-11]' }/>
-            <Heading size={ '4' }>You're all set!</Heading>
+            <Heading size={ '4' }>
+                { "You're all set!" }
+            </Heading>
             <Text color={ 'gray' } size={ '1' }>Please check your email to verify your account.</Text>
             <Button size={ '3' } onClick={ () => toggleModal() } className={ 'cursor-pointer w-full' }>Got it!</Button>
         </Flex>
