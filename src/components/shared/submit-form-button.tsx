81d44d77cd98ac3bbf86a90c384a7486337050ec
Scynes <dustin.greyfield@gmail.com>
2024-09-12 15:03:39 -0400

    add: submit-form button component for utilizing useFormState() hook.

---
+++ src/components/shared/submit-form-button.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Button } from '@radix-ui/themes';
+import { useFormStatus } from 'react-dom';
+
+interface Properties {
+
+    children: React.ReactNode;
+}
+
+export default ( { children }: Properties ) => {
+
+    const { pending } = useFormStatus()
+
+    return (
+        <Button type={ 'submit' } size={ '3' } className={ 'w-full cursor-pointer' } loading={ pending }>
+            { children }
+        </Button>
+    );
+}
